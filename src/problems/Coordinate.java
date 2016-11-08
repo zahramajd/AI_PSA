75fdd9be5d015a3931e7b4cfa162a856f87b231c
zahramajd <zahra.majdabadi95@gmail.com>
2016-11-09 01:28:16 +0330

    last

---
+++ src/problems/Coordinate.java
@@ -0,0 +1,16 @@
+package problems;
+
+/**
+ * Created by zahra on 11/8/2016 AD.
+ */
+public class Coordinate {
+
+    int state;
+    int x;
+    int y;
+
+    public Coordinate(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
