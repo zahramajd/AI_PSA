4e3a4f366a60d2bf526e97cd50bbe0e3594f04e8
zahramajd <zahra.majdabadi95@gmail.com>
2016-11-09 01:27:11 +0330

    last

---
+++ src/algorithms/DepthLimitedDFS.java
@@ -0,0 +1,103 @@
+package algorithms;
+
+import common.Node;
+import common.PSA;
+import common.Problem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zahra on 11/2/2016 AD.
+ */
+public class DepthLimitedDFS {
+
+
+    ArrayList<Node> result = new ArrayList<Node>();
+
+
+    public ArrayList<Node> solution(Node node) {
+        // TODO: return arraylist
+        ArrayList<Node> path = new ArrayList<>();
+        return path;
+    }
+
+
+    // tree
+
+    public ArrayList<Node> treeDepthLimitedSearch(Problem problem, int limit) {
+
+        return recursiveDLS(problem.initialState, problem, limit);
+    }
+
+
+    public ArrayList<Node> recursiveDLS(Node node, Problem problem, int limit) {
+
+        boolean cutoff_occurred;
+
+        if (problem.goal_test(node.state))
+            return solution(node);
+        else {
+            if (limit == 0)
+                return null;
+            else {
+                cutoff_occurred = false;
+                for (int i = 0; i < problem.actions(node.state).size(); i++) {
+                    Node child = PSA.child_node(problem, node, problem.actions(node.state).get(i));
+                    result = recursiveDLS(child, problem, limit - 1);
+                    if (result.size() == 0)
+                        cutoff_occurred = true;
+                    else {
+                        if (result != null)
+                            return result;
+                    }
+                }
+                if (cutoff_occurred)
+                    return result;
+                return null;
+            }
+        }
+
+    }
+
+    // Graph
+
+    ArrayList<Node> explored = new ArrayList<Node>();
+
+    public ArrayList<Node> graphDepthLimitedSearch(Problem problem, int limit) {
+
+        return graphRecursiveDLS(problem.initialState, problem, limit);
+    }
+
+
+    public ArrayList<Node> graphRecursiveDLS(Node node, Problem problem, int limit) {
+
+        boolean cutoff_occurred;
+
+        if (problem.goal_test(node.state))
+            return solution(node);
+        else {
+            if (limit == 0)
+                return null;
+            else {
+                cutoff_occurred = false;
+                for (int i = 0; i < problem.actions(node.state).size(); i++) {
+                    explored.add(node);
+                    Node child = PSA.child_node(problem, node, problem.actions(node.state).get(i));
+                    if (!explored.contains(child)) {
+                        result = recursiveDLS(child, problem, limit - 1);
+                        if (result.size() == 0)
+                            cutoff_occurred = true;
+                        else {
+                            if (result != null)
+                                return result;
+                        }
+                    }
+                }
+                if (cutoff_occurred)
+                    return result;
+                return null;
+            }
+        }
+
+    }
+}
